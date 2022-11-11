aeadaf6b9b5e30b779e910442596c8a8fffd9ae1
Esraa <esraargbali@gmail.com>
2022-11-11 16:32:32 +0200

    Task1

---
+++ Task1/parallelogram.js
@@ -0,0 +1,16 @@
+// calculating parallelogram Area
+
+// Algorithm A = Base * Height
+
+/*
+    1- Read Base, Height
+    2- Calculate Area by Multiplying Base by Height
+    3- Write The Output A 
+*/
+
+function Area(b, h) {
+  let a = b * h;
+  return a;
+}
+
+console.log(Area(10, 8));
---
+++ Task1/trapezoid.js
@@ -0,0 +1,16 @@
+// calculating parallelogram Area
+
+// Algorithm A = 0.5 (Base1 * Base2) * Height
+
+/*
+    1- Read Base1, Base2, Height
+    2- Calculate Area by Multiplying half of the two Bases summation by Height
+    3- Write The Output A 
+*/
+
+function Area(b1, b2, h) {
+  let a = 0.5 * (b1 + b2) * h;
+  return a;
+}
+
+console.log(Area(6, 2, 3));
